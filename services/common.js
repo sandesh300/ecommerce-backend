d1417ca2bf2eacbf57ad3d1b1ba3cfe920f6bd06
Sandesh Bhujbal <92014891+sandesh300@users.noreply.github.com>
2023-08-15 20:00:31 +0530

    Backend Part 10 - Email and Reset Password Flow

--- services/common.js
+++ services/common.js
@@ -1,4 +1,16 @@
 const passport = require('passport');
+const nodemailer = require('nodemailer');
+
+let transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // true for 465, false for other ports
+  auth: {
+    user: 'coderdost@gmail.com', // gmail
+    pass: process.env.MAIL_PASSWORD, // pass
+  },
+});
+
 
 exports.isAuth = (req, res, done) => {
   return passport.authenticate('jwt');
@@ -14,6 +26,18 @@ exports.cookieExtractor = function (req) {
     token = req.cookies['jwt'];
   }
   //TODO : this is temporary token for testing without cookie
-   // token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ZDc0NzFhMzUyMjg1YmIyMmQ3MDNkYSIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNjkxODMwMDQyfQ.egygBwkMezNO3ddI9Djeic-mD47iGsWQR6llRVTShSw'
+  // token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0NWJmNzViOWI3OWFkMTNiMTUxNWQ0MCIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNjgzNzQ4ODY3fQ.DyMEFgayuvUGrzvPHIxmCJWi4xstvp4hR-dRSBjRNhE"
   return token;
 };
+
+
+exports.sendMail = async function ({to, subject, text, html}){
+    let info = await transporter.sendMail({
+        from: '"E-commerce" <coderdost@gmail.com>', // sender address
+        to,
+        subject,
+        text,
+        html
+      });
+    return info;  
+}
